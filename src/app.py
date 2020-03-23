34654009ce5df2300b326e2de4bd9ca0a17aa235
Adam Stuller <adam.stuller@protonmail.com>
2020-03-23 12:38:59 +0100

    working on machine learning

--- src/app.py
+++ src/app.py
@@ -1,11 +1,17 @@
 from flask import Flask, request
+import os
+from dotenv import load_dotenv
+load_dotenv()
+
 app = Flask(__name__)
+
 
 @app.route('/classification/predict', methods=['POST'])
 def predict():
     app.logger.info(request.get_json())
     return request.get_json()
 
+
 if __name__ == "__main__":
-    app.config.from_object('config.DevelopmentConfig') 
+    app.config.from_object(os.environ['APP_SETTINGS'])
     app.run(host='0.0.0.0', port=5000)
--- src/config.py
+++ src/config.py
@@ -1,19 +1,21 @@
-class BaseConfig(object): 
-    'Base config class' 
-    DEBUG = True 
-    TESTING = False 
- 
-class ProductionConfig(BaseConfig): 
-    'Production specific config' 
-    DEBUG = False 
- 
-class StagingConfig(BaseConfig): 
-    'Staging specific config' 
-    DEBUG = True 
- 
-class DevelopmentConfig(BaseConfig): 
-    'Development environment specific config' 
-    DEBUG = True 
-    TESTING = True 
- 
+class BaseConfig(object):
+    'Base config class'
+    DEBUG = True
+    TESTING = False
 
+
+class ProductionConfig(BaseConfig):
+    'Production specific config'
+    DEBUG = False
+
+
+class StagingConfig(BaseConfig):
+    'Staging specific config'
+    DEBUG = True
+
+
+class DevelopmentConfig(BaseConfig):
+    'Development environment specific config'
+    DEBUG = True
+    TESTING = True
+    DEVELOPMENT = True
