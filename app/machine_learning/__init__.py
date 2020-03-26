a212dc57e95a86b76355638127fe6311fd1d8dd5
Adam Stuller <adam.stuller@protonmail.com>
2020-03-26 09:16:41 +0100

    new structure

--- app/machine_learning/__init__.py
+++
@@ -1,8 +0,0 @@
-from flask import Blueprint
-from os import path
-from joblib import load
-from app.config import config
-
-bp = Blueprint('machine_learning', __name__)
-# pipe = load(path.join(
-#     config['path_to_models'], 'pipeline-2020-03-05.joblib'))
