df89a5cec18a231485370e55d512b4e78b272104
Adam Stuller <adam.stuller@protonmail.com>
2020-03-28 15:53:55 +0100

    working on train

--- app/train.py
+++ app/train.py
@@ -2,10 +2,12 @@ from app.preprocessing import NLP4SKPreprocesser, TFIDFTransformer, LDATransform
 from sklearn.pipeline import Pipeline
 import pandas as pd
 from joblib import dump, load
-from os import path
+from os import path, listdir
+from os.path import isfile, splitext
 from app.config import config
 from sklearn.ensemble import RandomForestClassifier
 from datetime import date
+from app.constants import DATASET_ARG, PIPELINE_ARG
 
 import argparse
 
@@ -31,24 +33,70 @@ def get_data(data=None, path_to_dataset=path.join(config['path_to_datasets'], 'b
     return banks.head(20)
 
 
-def train_pipeline(path_to_dataset):
+def train_pipeline(dataset_name='banks', pipeline_name=None):
+
+    path_to_pipeline = path.join(
+        config['path_to_models'],
+        f'{pipeline_name}.joblib' if pipeline_name != None
+        else f'pipeline-{str(date.today())}.joblib')
+    path_to_dataset = path.join(
+        config['path_to_datasets'],
+        f'{dataset_name}.csv')
+
+    if not isfile(path_to_dataset):
+        raise FileNotFoundError(
+            'Dataset stated in dataset_name does not exists'
+            )
+
     pipe = Pipeline(
         steps=[
             ('nlp4sk', NLP4SKPreprocesser('sentence')),
             ('tf-idf', TFIDFTransformer('updated_sentence')),
             ('one-hot', OneHotTransformer(['parent_class'])),
             ('random_forest', RandomForestClassifier(n_estimators=200,
-                                                     max_depth=50, criterion='gini', bootstrap=True, random_state=42))
+                                                     max_depth=50,
+                                                     criterion='gini',
+                                                     bootstrap=True,
+                                                     random_state=42))
         ],
         verbose=True
     )
-    pipe.fit(get_data().drop(
-        columns=['class']), get_data(data=None, path_to_dataset=path_to_dataset)['class'])
-    dump(pipe, path.join(config['path_to_models'],
-                         f"pipeline-{str(date.today())}.joblib"))
+
+    pipe.fit(
+        get_data(data=None, path_to_dataset=path_to_dataset)
+        .drop(columns=['class']),
+        get_data(data=None, path_to_dataset=path_to_dataset)['class']
+    )
+
+    dump(pipe, path_to_pipeline)
+
+
+def get_all(folder_type):
+
+    if folder_type == DATASET_ARG:
+        path_to_pipelines = config['path_to_datasets']
+    elif folder_type == PIPELINE_ARG:
+        path_to_pipelines = config['path_to_models']
+    else:
+        raise KeyError(
+            f'folder_type argument is either "{DATASET_ARG}" or "{PIPELINE_ARG}"'
+        )
+
+    files_in_model_dir = list(
+        filter(
+            lambda f: isfile(path.join(path_to_pipelines, f)),
+            listdir(path_to_pipelines)
+        )
+    )
+    return list(
+        map(
+            lambda f: splitext(f)[0],
+            files_in_model_dir
+        )
+    )
 
 
 if __name__ == "__main__":
-    args = parse_arguments()
-    print(args)
-    train_pipeline(args.dataset_path)
+    # args = parse_arguments()
+    print(get_all('pipelines'))
+    # train_pipeline(args.dataset_path)
