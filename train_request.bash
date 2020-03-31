f1b541995789419ba6c7b0729ad8ff35a5426d72
Adam Stuller <adam.stuller@protonmail.com>
2020-03-31 20:32:09 +0200

    prod and dev docker-compose added

--- train_request.bash
+++ train_request.bash
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-curl http://127.0.0.1/api/v1/classification/training/pipeline \
+curl ec2-3-125-39-228.eu-central-1.compute.amazonaws.com/api/v1/classification/training/pipeline \
 -X POST  \
 -H "Content-Type:application/json" \
 -d '{
